0d647db4a1b4a4552c184cbb5c88249263805bf2
gvladislav-52 <gvladislav_52@mail.ru>
2024-06-08 23:30:22 +0300

    Create base project, removed storyboard references

---
+++ FoodDelApp/FoodDelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodDelApp
+//
+//  Created by macbookbro on 08.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
