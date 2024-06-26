c98b00e4fe45e237ce181311bcb0f3e61420d9cd
gvladislav-52 <gvladislav_52@mail.ru>
2024-06-26 12:40:57 +0300

    Fixed bug, when select page before ending animations

--- FoodDelApp/FoodDelApp/UILayer/Onboarding/OnboardingViewController.swift
+++ FoodDelApp/FoodDelApp/UILayer/Onboarding/OnboardingViewController.swift
@@ -12,7 +12,7 @@ class OnboardingViewController: UIViewController {
 
     //MARK: - Properties
     private var pages = [OnboardingPartViewController]()
-    
+    private var currentPageIndex = 0
     //MARK: - Views
     private let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
     private let pageControl = UIPageControl ()
@@ -134,8 +134,14 @@ extension OnboardingViewController: UIPageViewControllerDataSource {
 extension OnboardingViewController: UIPageViewControllerDelegate {
     func pageViewController(_ pageViewController: UIPageViewController, willTransitionTo pendingViewControllers: [UIViewController]) {
         if let index = pages.firstIndex(of: pendingViewControllers.first! as! OnboardingPartViewController) {
-            pageControl.currentPage = index
-            let page = pages[index]
+            currentPageIndex = index
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if completed {
+            pageControl.currentPage = currentPageIndex
+            let page = pages[currentPageIndex]
             let title = page.buttonText
             bottomButton.setTitle(title, for: .normal)
         }
