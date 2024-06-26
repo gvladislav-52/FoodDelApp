6816256c48e236c3427962ac0d037fbbf873b2fc
gvladislav-52 <gvladislav_52@mail.ru>
2024-06-26 12:38:48 +0300

    Added Factory

--- FoodDelApp/FoodDelApp/NavigationLayer/Coordinators/OnboardingCoordinate.swift
+++ FoodDelApp/FoodDelApp/NavigationLayer/Coordinators/OnboardingCoordinate.swift
@@ -7,7 +7,13 @@
 
 import UIKit
 
+//MARK: - OnboardingCoordinator
 class OnboardingCoordinator: Coordinator {
+    
+    //MARK: - Properties
+    private let factory = SceneFactory.self
+    
+    //MARK: - Methods
     override func start() {
         showOnboarding()
     }
@@ -17,40 +23,10 @@ class OnboardingCoordinator: Coordinator {
     }
 }
 
+//MARK: - Navigation
 private extension OnboardingCoordinator {
     func showOnboarding() {
-        var pages = [OnboardingPartViewController]()
-        let firstVC = OnboardingPartViewController()
-        firstVC.imageToShow = UIImage(imageLiteralResourceName: "chicken-leg 1")
-        firstVC.titleText = String("Delicious Food")
-        firstVC.discriptionText = String("Lorem ipsum dolor sit amet, consectetur adipiscing edit.")
-        firstVC.buttonText = String("Next")
-        
-        let secondVC = OnboardingPartViewController()
-        secondVC.imageToShow = UIImage(imageLiteralResourceName: "credit-card 1")
-        secondVC.titleText = String("Fast Shipping")
-        secondVC.discriptionText = String("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum rhoncus nulla.")
-        secondVC.buttonText = String("Next")
-        
-        let thirdVC = OnboardingPartViewController()
-        thirdVC.imageToShow = UIImage(imageLiteralResourceName: "medal 1")
-        thirdVC.titleText = String("Certificate Food")
-        thirdVC.discriptionText = String("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies mauris a id.")
-        thirdVC.buttonText = String("Next")
-        
-        let fourVC = OnboardingPartViewController()
-        fourVC.imageToShow = UIImage(imageLiteralResourceName: "credit-card 1")
-        fourVC.titleText = String("Payment Online")
-        fourVC.discriptionText = String("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui ultricies sit massa.")
-        fourVC.buttonText = String("Cool!")
-        
-        pages.append(firstVC)
-        pages.append(secondVC)
-        pages.append(thirdVC)
-        pages.append(fourVC)
-        
-        let presenter = OnboardingViewPresenter(coordinator: self)
-        let viewController = OnboardingViewController(pages: pages, viewOutput: presenter)
+        let viewController = factory.makeOnboardingScene(coordinator: self)
         navigationController?.pushViewController(viewController, animated: true)
     }
 }
