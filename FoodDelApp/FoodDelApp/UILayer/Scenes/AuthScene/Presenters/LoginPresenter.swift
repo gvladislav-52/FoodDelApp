5eac1a3d5ea0c91c8ec8f2ad70a37c842005aaf0
gvladislav-52 <gvladislav_52@mail.ru>
2024-06-28 18:37:06 +0300

    Added custom FDTextField

---
+++ FoodDelApp/FoodDelApp/UILayer/Scenes/AuthScene/Presenters/LoginPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewPresenter.swift
+//  FoodDelApp
+//
+//  Created by macbookbro on 28.06.2024.
+//
+
+import Foundation
+
+class LoginPresenter {
+    
+}
---
+++ FoodDelApp/FoodDelApp/UILayer/Views/FoodDeliveryTextField.swift
@@ -0,0 +1,20 @@
+//
+//  FoodDeliveryTextField.swift
+//  FoodDelApp
+//
+//  Created by macbookbro on 27.06.2024.
+//
+
+import UIKit
+
+class FoodDeliveryTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
